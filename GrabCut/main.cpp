eb09e9680edbd10802e0b17c04e256274a807506
qq456cvb <447626601@qq.com>
2018-09-01 17:10:11 +0800

    Initial Commit

---
+++ GrabCut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GrabCut
+//
+//  Created by Neil on 2018/9/1.
+//  Copyright © 2018 Neil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
